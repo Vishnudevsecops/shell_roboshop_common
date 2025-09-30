8bebb7739925ca9c3b69eade6edd71417247b7c4
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-30 11:07:20 +0530

    user cart files created

---
+++ cart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ./common.sh
+#calling common.sh file to avoid code redundancy    
+cehck_rootaccess
+#check if current user has root access  
+
+app_name="cart" 
+app_setup
+#calling app_setup function from common.sh      
+nodejs_installation
+#calling nodejs installation function from common.sh    
+
+systemd_setup
+#calling systemd setup function from common.sh
+app_restart
+#calling app_restart function from common.sh
